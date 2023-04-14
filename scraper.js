73cb3b178e00fc084effc9a51ebb922ff2b024a2
Rich Levy <rich.600@gmail.com>
2023-04-14 14:17:53 +0100

    Updated current house drawn and total house consumption selectors [thanks Yetkin]

--- scraper.js
+++ scraper.js
@@ -132,11 +132,11 @@ async function scrapeData() {
 
 
 		// House draw now
-		const currentHouseDrawElement = await popup.$('.animation > .wrap > .yongdian > .content > span')
+		const currentHouseDrawElement = await popup.$('.animation > .wrap > .grid-side > .content > span')
 		const currentHouseDraw = await (await currentHouseDrawElement.getProperty('textContent')).jsonValue()
 
 		// House consumption today
-		const totalHouseConsumptionElement = await popup.$('.bottomtext-info > div > .yongdian-info > div > span:nth-child(2)')
+		const totalHouseConsumptionElement = await popup.$('.animation > .wrap > .grid-side > .use-power > span:nth-child(2)')
 		const totalHouseConsumption = await (await totalHouseConsumptionElement.getProperty('textContent')).jsonValue()
 
 		await browser.close()
